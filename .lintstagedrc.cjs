a1df1b3d7670e2fd1d0b9713788f9b9e65f623ae
onota0318 <onot0318@gmail.com>
2024-12-28 14:33:22 +0900

    first commit

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,8 @@
+// @ts-check
+
+// @ts-check
+module.exports = {
+    "*.{js,cjs,ts,jsx,tsx,astro}": ["eslint --fix", "prettier --write"],
+    "*.{md,html,json,yaml,yml}": ["prettier --write"],
+  }
+  
